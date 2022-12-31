04282d25a27ac9b1d7163149ba10c8a89c4314d4
Fidel <fidelotieno11@gmail.com>
2022-12-31 12:45:35 +0300

    Added First JS files
    Did this as i started this JavaScript Repository on 31st Dec 2022

---
+++ destructuring.js
@@ -0,0 +1,10 @@
+//with destructuring
+const objects = ['table', 'iphone','apple','mansion']
+const [furniture,mobile,fruit,home]= objects
+//without destructuring
+//const furniture=objects[0];
+//const mobile=objects[1];
+//const fruit=objects[2];
+//const home=objects[3];
+
+console.log(home);
---
+++ ternaryoperator.js
@@ -0,0 +1,2 @@
+//console.log("HELL NO");
+(15<10) ? console.log("Hello user") : console.log("Welcome aboard")
